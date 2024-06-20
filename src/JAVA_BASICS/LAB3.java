2fb0ead215c209163abfe2e683f5c9a737c72766
Rakesh R <rakeshrake5398@gmail,com>
2024-06-20 19:45:27 +0530

    new commit

--- src/JAVA_BASICS/LAB3.java
+++ src/JAVA_BASICS/LAB3.java
@@ -1,4 +1,14 @@
 package JAVA_BASICS;
 
 public class LAB3 {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(++a);
+        System.out.println(--a);
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(a++ + a--);
+        System.out.println(a-- + a++);
+        System.out.println(++a + a++ + -a);
+    }
 }
