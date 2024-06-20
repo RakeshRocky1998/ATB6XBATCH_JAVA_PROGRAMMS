fc77432306e79b5943e4b0bb8b547525b77b7a5c
Rakesh R <rakeshrake5398@gmail,com>
2024-06-20 13:44:38 +0530

    new commit

--- src/JAVA_BASICS/String_1.java
+++ src/JAVA_BASICS/String_1.java
@@ -3,21 +3,15 @@ package JAVA_BASICS;
 public class String_1 {
     public static void main(String[] args) {
 
-    String a = "RRR";
-    String b = "RRR";
-    String c = "ZZZ";
-    String d = "rrr";
+   // String a = "RRR";String b = "RRR";String c = "ZZZ";String d = "rrr";
+    String a = "RRR", b = "RRR", c = "ZZZ", d = "rrr";
 
         System.out.println(a.equals(b));
         System.out.println(a == b);
-
         System.out.println("   ");
-
         System.out.println(a.equals(c));
         System.out.println(a == c);
-
         System.out.println("   ");
-
         System.out.println(a.equals(d));
         System.out.println(a == d);
 
