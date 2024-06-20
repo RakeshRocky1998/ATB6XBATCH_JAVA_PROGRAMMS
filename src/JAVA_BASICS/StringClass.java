ca725afbafab1aa04ba1e25d5f16435238fc3d12
Rakesh R <rakeshrake5398@gmail,com>
2024-06-20 08:41:47 +0530

    new commit

---
+++ src/JAVA_BASICS/StringClass.java
@@ -0,0 +1,16 @@
+package JAVA_BASICS;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class StringClass {
+    public static void main(String[] args) {
+
+
+        String a = "Kkkk";
+        String b = new String("Rrrrrrr");
+
+        System.out.println(a+b);
+
+
+    }
+}
