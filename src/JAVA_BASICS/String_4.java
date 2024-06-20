ba237668565bf3a733f0b18ddc03d9980e01ec77
Rakesh R <rakeshrake5398@gmail,com>
2024-06-20 13:51:13 +0530

    new commit

---
+++ src/JAVA_BASICS/String_4.java
@@ -0,0 +1,12 @@
+package JAVA_BASICS;
+
+public class String_4 {
+    public static void main(String[] args) {
+        String a = "Rakesh";
+        String b = "Rakesh ";
+        System.out.println(a.equals(b));
+        System.out.println(a==b);
+        System.out.println(a.charAt(0));
+
+    }
+}
