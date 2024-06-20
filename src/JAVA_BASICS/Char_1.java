f3bd94dfd8eeb5e539961bed4612f0bf011b01f1
Rakesh R <rakeshrake5398@gmail,com>
2024-06-20 14:08:47 +0530

    new commit

---
+++ src/JAVA_BASICS/Char_1.java
@@ -0,0 +1,13 @@
+package JAVA_BASICS;
+
+public class Char_1 {
+    public static void main(String[] args) {
+        String a = "RAKESH";
+        char b = a.charAt(1);
+        System.out.println(b);
+        System.out.println(a.contains("s"));
+        System.out.println(a.contains("S"));
+        System.out.println(a.contains("a"));
+        System.out.println(a.contains("A"));
+    }
+}
