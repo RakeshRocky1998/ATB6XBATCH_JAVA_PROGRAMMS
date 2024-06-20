3c675ffa38904c3e653221985b8d48adea1d8ac6
Rakesh R <rakeshrake5398@gmail,com>
2024-06-20 14:33:29 +0530

    new commit

---
+++ src/JAVA_BASICS/String_5.java
@@ -0,0 +1,10 @@
+package JAVA_BASICS;
+
+public class String_5 {
+    public static void main(String[] args) {
+        String a = "Rakhi";
+        a.concat(" Shaiva");
+        System.out.println(a);
+        System.out.println(a.concat(" Shaiva"));
+    }
+}
