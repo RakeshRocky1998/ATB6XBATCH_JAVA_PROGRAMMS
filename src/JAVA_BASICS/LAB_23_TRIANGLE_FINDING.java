c8c8c2ad973c9cf9c1dc329613a22d1b6c35790c
Rakesh R <rakeshrake5398@gmail,com>
2024-06-22 13:23:40 +0530

    new commit

---
+++ src/JAVA_BASICS/LAB_23_TRIANGLE_FINDING.java
@@ -0,0 +1,24 @@
+package JAVA_BASICS;
+
+import java.util.Scanner;
+
+public class LAB_23_TRIANGLE_FINDING {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the sides of triangle SIDE1");
+        int side1 = sc.nextInt();
+        System.out.println("Enter the sides of triangle SIDE2");
+        int side2 = sc.nextInt();
+        System.out.println("Enter the sides of triangle SIDE3");
+        int side3 = sc.nextInt();
+
+
+        if ((side1 == side2) && (side2==side3) && (side3==side1)){
+            System.out.println("EQUILATERAL TRIANGLE");
+        } else if ((side1==side2) || (side2==side3)|| (side3==side1)) {
+            System.out.println("ISOCELESE TRIANGLE");
+        }else {
+            System.out.println("SCALENE TRIANGLE");
+        }
+    }
+}
