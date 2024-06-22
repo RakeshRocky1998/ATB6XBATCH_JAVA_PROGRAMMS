79ae27005cd312a82e3fca9b8cb8222a0620faba
Rakesh R <rakeshrake5398@gmail,com>
2024-06-22 12:38:56 +0530

    new commit

---
+++ src/JAVA_BASICS/LAB_22_.java
@@ -0,0 +1,18 @@
+package JAVA_BASICS;
+
+public class LAB_22_ {
+    public static void main(String[] args) {
+
+        if (true) {
+            System.out.println("1");
+            if (true) {
+                System.out.println("2");
+                if (true) {
+                    System.out.println("3");
+                }
+            }
+        }
+
+
+    }
+}
