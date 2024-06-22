3df8cce37fb8642b074e9297fa8433017b990da6
Rakesh R <rakeshrake5398@gmail,com>
2024-06-22 17:12:53 +0530

    new commit

---
+++ src/JAVA_BASICS/LAB_24_FIZZBUZZ.java
@@ -0,0 +1,24 @@
+package JAVA_BASICS;
+
+import java.util.Scanner;
+
+public class LAB_24_FIZZBUZZ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
+        int a = sc.nextInt();
+        if (a >= 3) {
+            if (a % 3 == 0 && a % 5 == 0) {
+                System.out.println("FIZZ----BUZZ");
+            } else if (a % 5 == 0) {
+                System.out.println("BUZZ");
+            } else if (a % 3 == 0) {
+                System.out.println("FIZZ");
+            } else {
+                System.out.println("NO FIZZ NO BUZZ");
+            }
+        }else{
+            System.out.println("IDIOT ENTER PROPER NUMBER");
+        }sc.close();
+    }
+}
