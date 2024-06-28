fa7404b43a41fa29e4d2f425a3614d2c54ec30ad
Rakesh R <rakeshrake5398@gmail,com>
2024-06-28 18:43:52 +0530

    commit

--- src/JAVA_BASICS/_51_75/LAB_100.java
+++ src/JAVA_BASICS/_51_75/LAB_100.java
@@ -1,4 +1,10 @@
 package JAVA_BASICS._51_75;
 
 public class LAB_100 {
+    public static void main(String[] args) {
+        System.out.println("jii");
+        System.out.println("jii");
+
+    }
+
 }
