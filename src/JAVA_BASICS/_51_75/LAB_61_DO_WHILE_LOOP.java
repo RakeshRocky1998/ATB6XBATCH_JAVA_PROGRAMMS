12cb2027d1a5170c7c0c7eb6200f711daaf2fa4b
Rakesh R <rakeshrake5398@gmail,com>
2024-06-28 19:39:46 +0530

    commit

---
+++ src/JAVA_BASICS/_51_75/LAB_61_DO_WHILE_LOOP.java
@@ -0,0 +1,18 @@
+package JAVA_BASICS._51_75;
+
+public class LAB_61_DO_WHILE_LOOP {
+    public static void main(String[] args) {
+
+        int a = 10;
+        do {
+            System.out.println(a);
+            a++;
+        }while (a<10);
+
+        int b = 10;
+        while (b <10){
+            System.out.println(b);
+            b++;
+        }
+    }
+}
